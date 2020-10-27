726c4c424a6e4c92dc818fdead6475cda9aafdf8
sivchari <shibuuuu5@gmail.com>
2020-10-28 02:54:19 +0900

    Update テーブル修正
    userテーブルを親と子に分離。
    現状保留の内容に関するテーブルと紐付く子テーブルのカラムをコメントアウト

--- migrations/20201019153137-create_table.sql
+++ migrations/20201019153137-create_table.sql
@@ -1,49 +1,76 @@
 -- +migrate Up
 
 -- ------------------------------------------------
--- Table`users`
+-- Table`user`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(254) NOT NULL DEFAULT 'NONE',
     `password` VARCHAR(100) NOT NULL DEFAULT 'NONE',
     `social_id` VARCHAR(50) NOT NULL DEFAULT 'NONE',
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `deleted_at` DATETIME NOT NULL DEFAULT 'NONE',
+    PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- ------------------------------------------------
+-- Table`login`
+-- ------------------------------------------------
+CREATE TABLE IF NOT EXISTS `login` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
     `last_login` DATETIME NOT NULL,
-    `continue_days` INT NOT NULL DEFAULT 0,
+    `continuation` INT NOT NULL DEFAULT 0,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 -- ------------------------------------------------
--- Table`points`
+-- Table`profile`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `points` (
+CREATE TABLE IF NOT EXISTS `profile` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `avatar` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+-- ------------------------------------------------
+-- Table`point`
+-- ------------------------------------------------
+CREATE TABLE IF NOT EXISTS `point` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `match_id` INT NOT NULL,
     `user_id` INT NOT NULL,
     `bet_id` INT NOT NULL,
     `origin_id` INT NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+-- originはlog関数作成で破棄
+
 -- ------------------------------------------------
--- Table`origins`
+-- Table`origin`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `origins` (
+CREATE TABLE IF NOT EXISTS `origin` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+-- scoreとmatchはAPIのResponse内容で変更確定
+
 -- ------------------------------------------------
--- Table`scores`
+-- Table`score`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `scores` (
+CREATE TABLE IF NOT EXISTS `score` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `sport_id` INT NOT NULL,
     `match_id` INT NOT NULL,
@@ -56,9 +83,9 @@ CREATE TABLE IF NOT EXISTS `scores` (
 ENGINE = InnoDB;
 
 -- ------------------------------------------------
--- Table`matches`
+-- Table`match`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `matches` (
+CREATE TABLE IF NOT EXISTS `match` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `league` VARCHAR(10) NOT NULL,
     `tournament` VARCHAR(30) NOT NULL,
@@ -71,36 +98,37 @@ CREATE TABLE IF NOT EXISTS `matches` (
 ENGINE = InnoDB;
 
 -- ------------------------------------------------
--- Table`sports`
+-- Table`sport`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `sports` (
+CREATE TABLE IF NOT EXISTS `sport` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(20) NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 -- ------------------------------------------------
--- Table`bets`
+-- Table`bet`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bets` (
+CREATE TABLE IF NOT EXISTS `bet` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `sport_id` INT NOT NULL,
     `match_id` INT NOT NULL,
     `latch` INT NOT NULL,
     `odds` DECIMAL NOT NULL,
     `pay_done` BOOLEAN NOT NULL DEFAULT FALSE,
-    `content_id` INT NOT NULL,
+    -- `content_id` INT NOT NULL,
     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
+-- 保留
+
 -- ------------------------------------------------
 -- Table`bet_contents`
 -- ------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bet_contents` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(50) NOT NULL,
-    `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+-- CREATE TABLE IF NOT EXISTS `bet_contents` (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `name` VARCHAR(50) NOT NULL,
+--     `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     PRIMARY KEY (`id`))
+-- ENGINE = InnoDB;
